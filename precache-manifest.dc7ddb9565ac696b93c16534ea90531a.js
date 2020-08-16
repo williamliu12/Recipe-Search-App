f89fb7660839a183f4a3f8803f45c671343d6331
William Liu <wliu134@asu.edu>
2020-08-16 16:53:16 -0700

    Updates

---
+++ precache-manifest.dc7ddb9565ac696b93c16534ea90531a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49df64282e6fcdc808a62e668621d4b9",
+    "url": "/Recipe-Search-App/index.html"
+  },
+  {
+    "revision": "1a392ccda6c2ffad0814",
+    "url": "/Recipe-Search-App/static/css/main.a8842704.chunk.css"
+  },
+  {
+    "revision": "3a118cb7c9f53895e394",
+    "url": "/Recipe-Search-App/static/js/2.db53df5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-Search-App/static/js/2.db53df5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a392ccda6c2ffad0814",
+    "url": "/Recipe-Search-App/static/js/main.cbc83d6f.chunk.js"
+  },
+  {
+    "revision": "fa52844c42eff9ea5dae",
+    "url": "/Recipe-Search-App/static/js/runtime-main.766221bb.js"
+  }
+]);
